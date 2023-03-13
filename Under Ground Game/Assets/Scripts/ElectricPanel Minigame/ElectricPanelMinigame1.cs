915eeee0c51c27eeb8e451f8d290371484b7ecaa
Augussto <augusstocs@gmail.com>
2023-03-13 17:16:34 -0300

    correciones minijuego del panel electrico

--- Under Ground Game/Assets/Scripts/ElectricPanel Minigame/ElectricPanelMinigame1.cs
+++ Under Ground Game/Assets/Scripts/ElectricPanel Minigame/ElectricPanelMinigame1.cs
@@ -4,23 +4,46 @@ using UnityEngine;
 
 public class ElectricPanelMinigame1 : MonoBehaviour
 {
-    public GameObject lever1, lever2, lever3, lever4;
+    //public GameObject lever1, lever2, lever3, lever4;
+    Vector3 mousePosition;
+    public Light indicatorLight;
 
-    // Start is called before the first frame update
-    void Start()
+
+    private Vector3 GetMousePos()
     {
+        return Camera.main.WorldToScreenPoint(transform.position);
+    }
+    private void OnMouseDown()
+    {
+        mousePosition = Input.mousePosition - GetMousePos();
+    }
+    private void OnMouseDrag()
+    {
+        Vector3 mouseMovement = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePosition);
+        if (mouseMovement.y < 2.4 & mouseMovement.y > 1.9)
+        {
+            transform.position = new Vector3(transform.position.x, mouseMovement.y, transform.position.z);
+            indicatorLight.color = Color.red;
 
+        } 
+        else if(mouseMovement.y > 2.4)
+        {
+            indicatorLight.color = Color.green;
+            Debug.Log("LLEGO A POSICION CORRECTA, pos y: " + mouseMovement.y);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Only for test
         if (Input.GetMouseButtonDown(1))
         {
             Debug.Log("Luces fuera");
             turnOffLights();
         }
-        //Cursor.visible = false;
+        // 
+        /*
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
@@ -57,14 +80,12 @@ public class ElectricPanelMinigame1 : MonoBehaviour
                     }
                 }
             }
-        }
+        }*/
     }
 
     public void turnOffLights()
     {
-        lever1.transform.localPosition = new Vector3(-0.9f, Random.Range(-0.260f,0.125f), 0.344f);
-        lever2.transform.localPosition = new Vector3(-0.9f, Random.Range(-0.260f, 0.125f), 0.141f);
-        lever3.transform.localPosition = new Vector3(-0.9f, Random.Range(-0.260f, 0.125f), -0.141f);
-        lever4.transform.localPosition = new Vector3(-0.9f, Random.Range(-0.260f, 0.125f), -0.344f);
+        transform.localPosition = new Vector3(-0.9f, Random.Range(-0.260f,0.125f), transform.localPosition.z);
+        indicatorLight.color = Color.red;
     }
 }
