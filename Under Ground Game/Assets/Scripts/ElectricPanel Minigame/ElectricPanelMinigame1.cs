d2c24ed7bf9613fbadc8f951d3a118241236fb0f
Augussto <augusstocs@gmail.com>
2023-03-16 16:40:42 -0300

    Panel electrico en la sample scene

--- Under Ground Game/Assets/Scripts/ElectricPanel Minigame/ElectricPanelMinigame1.cs
+++ Under Ground Game/Assets/Scripts/ElectricPanel Minigame/ElectricPanelMinigame1.cs
@@ -7,11 +7,12 @@ public class ElectricPanelMinigame1 : MonoBehaviour
     //public GameObject lever1, lever2, lever3, lever4;
     Vector3 mousePosition;
     public Light indicatorLight;
+    public Camera cam;
 
 
     private Vector3 GetMousePos()
     {
-        return Camera.main.WorldToScreenPoint(transform.position);
+        return cam.WorldToScreenPoint(transform.position);
     }
     private void OnMouseDown()
     {
@@ -19,18 +20,19 @@ public class ElectricPanelMinigame1 : MonoBehaviour
     }
     private void OnMouseDrag()
     {
-        Vector3 mouseMovement = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePosition);
-        if (mouseMovement.y < 2.4 & mouseMovement.y > 1.9)
+        Vector3 mouseMovement = cam.ScreenToWorldPoint(Input.mousePosition - mousePosition);
+        if (transform.localPosition.y < 0.24 & mouseMovement.y > 1.9)
         {
             transform.position = new Vector3(transform.position.x, mouseMovement.y, transform.position.z);
             indicatorLight.color = Color.red;
 
         } 
-        else if(mouseMovement.y > 2.4)
+        else if(transform.localPosition.y > 0.24)
         {
             indicatorLight.color = Color.green;
             Debug.Log("LLEGO A POSICION CORRECTA, pos y: " + mouseMovement.y);
         }
+        Debug.Log("pos y: " + transform.localPosition.y);
     }
 
     // Update is called once per frame
@@ -42,45 +44,6 @@ public class ElectricPanelMinigame1 : MonoBehaviour
             Debug.Log("Luces fuera");
             turnOffLights();
         }
-        // 
-        /*
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                if (hit.transform.name == "Lever 1")
-                {
-                    if(lever1.transform.localPosition.y < 0.260)
-                    {
-                        lever1.transform.localPosition = new Vector3(-0.9f, lever1.transform.localPosition.y + 0.015f, 0.344f);
-                    }
-                } 
-                else if (hit.transform.name == "Lever 2")
-                {
-                    if (lever2.transform.localPosition.y < 0.260)
-                    {
-                        lever2.transform.localPosition = new Vector3(-0.9f, lever2.transform.localPosition.y + 0.015f, 0.141f);
-                    }
-                }
-                else if (hit.transform.name == "Lever 3")
-                {
-                    if (lever3.transform.localPosition.y < 0.260)
-                    {
-                        lever3.transform.localPosition = new Vector3(-0.9f, lever3.transform.localPosition.y + 0.015f, -0.141f);
-                    }
-                }
-                else if (hit.transform.name == "Lever 4")
-                {
-                    if (lever4.transform.localPosition.y < 0.260)
-                    {
-                        lever4.transform.localPosition = new Vector3(-0.9f, lever4.transform.localPosition.y + 0.015f, -0.344f);
-                    }
-                }
-            }
-        }*/
     }
 
     public void turnOffLights()
